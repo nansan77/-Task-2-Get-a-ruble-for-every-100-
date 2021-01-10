993b6c0009b7fbb19b220edd60c19d75bcff7c1a
Aleksandr Nikitin <nan.alek@yandex.ru>
2021-01-10 11:53:48 +0300

    Начисления бонуса

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
         boolean topup = false;
         int adding;
         if (topup) {
-            adding = 1000;}
-        else  {
-            adding = -1; }
-
-
+            adding = 1000;
+        } else {
+            adding = 100;
+        }
         int every = 100;
-        int bonus = adding /every;
-
-
+        int limit = 1000;
+        int bonus = adding / every;
+        if (adding < limit) {
+            bonus = 0;
+        }
         System.out.println(bonus);
 
 
